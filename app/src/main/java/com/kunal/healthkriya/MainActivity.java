23f068ec58b3121b44190cdf9875f371e0d7b5e5
Kunal singh <kunalsingh9355@gmail.com>
2026-02-15 09:40:17 +0530

    updated bottom navigation bar and size

--- app/src/main/java/com/kunal/healthkriya/MainActivity.java
+++ app/src/main/java/com/kunal/healthkriya/MainActivity.java
@@ -19,20 +19,22 @@ public class MainActivity extends AppCompatActivity {
 
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.nav_host);
+                        .findFragmentById(R.id.nav_host_fragment);
 
-        NavController navController = navHostFragment.getNavController();
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
-        NavigationUI.setupWithNavController(bottomNav, navController);
+            BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+            NavigationUI.setupWithNavController(bottomNav, navController);
 
-        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            int id = destination.getId();
-            boolean showBottomNav = id == R.id.homeFragment
-                    || id == R.id.myHealthFragment
-                    || id == R.id.remindersFragment
-                    || id == R.id.profileFragment;
-            bottomNav.setVisibility(showBottomNav ? View.VISIBLE : View.GONE);
-        });
+            navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+                int id = destination.getId();
+                boolean showBottomNav = id == R.id.homeFragment
+                        || id == R.id.myHealthFragment
+                        || id == R.id.remindersFragment
+                        || id == R.id.profileFragment;
+                bottomNav.setVisibility(showBottomNav ? View.VISIBLE : View.GONE);
+            });
+        }
     }
 }
